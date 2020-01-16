5cc50e3416e50c351e922acba54a22183c406cd3
Gideon-Felt <gideon@feltslanding.com>
2020-01-15 18:29:23 -0700

    finished Portfolio manager, and built api call for blog, and implemented infinate scrolling in vanilla JS

--- ReactPortfolio/src/components/pages/blog.js
+++ ReactPortfolio/src/components/pages/blog.js
@@ -1,13 +1,107 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios"
 
-export default function() {
-    return (
-        <div>
-            <h2>Blog</h2>
-            <div>
-                <Link to="/about-me">read more about me</Link>
+
+import BlogItem from "../blog/blog-item"
+import BlogModal from "../modals/blog-modal"
+
+class Blog extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            blogItems: [],
+            totalCount: 0,
+            currentPage: 0,
+            isLoading: true,
+            blogModalIsOpen: false
+        }
+
+        this.getBlogItems = this.getBlogItems.bind(this)
+        this.onScroll = this.onScroll.bind(this)
+        window.addEventListener("scroll", this.onScroll, false)
+        this.handleNewBlogClick = this.handleNewBlogClick.bind(this)
+        this.handleModalClose = this.handleModalClose.bind(this)
+    }
+
+    handleNewBlogClick() {
+        this.setState({
+            blogModalIsOpen: true
+        })
+    }
+
+    handleModalClose() {
+        this.setState({
+            blogModalIsOpen: false
+        })
+    }
+
+    
+
+    onScroll() {
+        if (this.state.isLoading || this.state.blogItems.length === this.setState.totalCount) {
+            return
+        }
+
+        if (window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight) {
+            this.getBlogItems()
+        }
+    }
+
+
+    getBlogItems() {
+        this.setState({
+            currentPage: this.state.currentPage + 1
+        })
+        axios.get(`https://gdfelt.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`,
+        {withCredentials: true
+        }).then(response => {
+            console.log("getting", response.data);
+         this.setState({
+            blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+            totalCount: response.data.meta.total_records,
+            isLoading: false
+         })
+        }).catch(error => {
+            console.log("getBlogItems error", error);
+            
+        })
+    }
+
+    componentWillMount() {
+        this.getBlogItems()
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("scroll", this.onScroll, false)
+    }
+
+    render() {
+
+        const blogRecords = this.state.blogItems.map(blogItem => {
+        return <BlogItem key={blogItem.id} blogItem={blogItem} />
+        })
+
+        return (
+            <div className="blog-container">
+                <BlogModal 
+                handleModalClose={this.handleModalClose}
+                isOpen={this.state.blogModalIsOpen}/>
+
+                <div className="new-blog-link">
+                    <a onClick={this.handleNewBlogClick}>Open Modal!</a>
+                </div>
+
+                <div className="content-container">{blogRecords}</div>
+                <div className="content-loader">
+                    {this.state.isLoading ? <FontAwesomeIcon icon="spinner" spin/> : null}
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    
 }
+
+export default Blog
