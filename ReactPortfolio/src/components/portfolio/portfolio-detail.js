500e4277757380893846a628e7c5e78305c086d4
Gideon-Felt <gideon@feltslanding.com>
2020-01-20 23:49:13 -0700

    added about and contact pages, and built links for indevidual portfolio items

--- ReactPortfolio/src/components/portfolio/portfolio-detail.js
+++ ReactPortfolio/src/components/portfolio/portfolio-detail.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
 
-export default function(props) {
-    return (
-        <div>
-            <h2>Portfolio detail for {props.match.params.slug}</h2>
-        </div>
-    )
+export default class PortfolioDetail extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentWillMount() {
+        this.getPortfolioItem()
+    }
+
+    getPortfolioItem() {
+        Axios.get(`https://gdfelt.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`, { withCredentials: true }).then(response => {
+            console.log(response)
+        })
+        .catch(error => {
+            console.log("getPortfolioItem error", error)
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Portfolio detail for {this.props.match.params.slug}</h2>
+            </div>
+        )
+    }
+    
 }
